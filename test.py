d3c9bfc3e9a210c67934cec0bc6bf2489685d039
John O'Meara <john.fr.omeara@gmail.com>
2019-09-16 21:38:24 -0400

    make all parts parsed from variables (except location)

--- test.py
+++ test.py
@@ -5,11 +5,15 @@
 # first up, an ipv4 record
 name = "test.example.com"
 ttl = 86400
+ttd = 0
 address = "192.168.1.42"
 
 import sys
 out = sys.stdout.buffer
 
+RR_TYPE_A = 1
+RR_TYPE_TXT = 16
+RR_TYPE_AAAA = 28
 
 def ipv4_to_u32(ipv4):
     parts = ipv4.split('.')
@@ -21,13 +25,25 @@ def ipv4_to_u32(ipv4):
         res |= int(part)
     return res
 
-def u32_to_bytes(u32):
+def u_to_bytes(u, bits):
+    if bits & 0x7:
+        raise Exception("Extra bits; not byte aligned")
+    byte_count = bits >> 3
     res = []
-    while u32:
-        res.append(u32 & 0xff)
-        u32 >>= 8
+    for i in range(byte_count):
+        res.append(u & 0xff)
+        u >>= 8
     res.reverse()
     return bytes(res)
+
+def u16_to_bytes(u16):
+    return u_to_bytes(u16, 16)
+
+def u32_to_bytes(u32):
+    return u_to_bytes(u32, 32)
+
+def u64_to_bytes(u64):
+    return u_to_bytes(u64, 64)
 
 #print(bytes(u32_to_bytes(ipv4_to_u32(address))).hex(), flush=True)
 #out.write(bytes(u32_to_bytes(ipv4_to_u32(address))))
@@ -49,7 +65,7 @@ def labels_to_dns(labels):
     return b''.join(res)
 
 key = labels_to_dns(name_to_labels(name))
-value = bytes([0,1, ord('='), 0, 1, 0x51, 0x80, 0, 0, 0, 0, 0, 0, 0, 0]) + u32_to_bytes(ipv4_to_u32(address))
+value = u16_to_bytes(RR_TYPE_A) + b'=' + u32_to_bytes(ttl) + u64_to_bytes(ttd) + u32_to_bytes(ipv4_to_u32(address))
 klen = len(key)
 vlen = len(value)
 record = "+{},{}:".format(klen,vlen).encode('ascii') + key + b'->' + value + bytes((0x0a,))
