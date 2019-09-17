f8f640f22ea8406790bdce68c6849fcf41cbd5e9
John O'Meara <john.fr.omeara@gmail.com>
2019-09-16 22:59:21 -0400

    Add support for Z and &.
    This should be enough to actually serve a simple zone now.

--- test.py
+++ test.py
@@ -3,9 +3,21 @@
 # should be piped directly to cdbmake or cdb
 
 import sys
+import time
+import os
+
 out = sys.stdout.buffer
 
+default_TTL = "86400"
+#timestr = str(int(time.time())) # used for default SOA serial number
+timestr = str(int(os.stat('data').st_mtime)) # used for default SOA serial number
+
 RR_TYPE_A = 1
+RR_TYPE_NS = 2
+RR_TYPE_CNAME = 5
+RR_TYPE_SOA = 6
+RR_TYPE_PTR = 12
+RR_TYPE_MX = 15
 RR_TYPE_TXT = 16
 RR_TYPE_AAAA = 28
 
@@ -89,6 +101,7 @@ def make_record(name, type_, loc, ttl, ttd, data):
     record = "+{},{}:".format(klen,vlen).encode('ascii') + key + b'->' + value + bytes((0x0a,))
     return record
 
+
 with open("data") as data:
     for line in data:
         line = line.rstrip()
@@ -97,7 +110,8 @@ with open("data") as data:
         rtype = line[0]
         line = line[1:]
         if rtype == '+':
-            defaults = [None, None, "86400", "0", None]
+            # Address
+            defaults = [None, None, default_TTL, "0", None]
             givenfields = line.split(':')
             fields = overlay(givenfields, defaults)
             name = fields[0]
@@ -107,6 +121,61 @@ with open("data") as data:
             loc = fields[4]
             data = u32_to_bytes(ipv4_to_u32(address))
             out.write(make_record(name, RR_TYPE_A, loc, ttl, ttd, data))
+        elif rtype == '-':
+            # Disabled A record
+            continue
+        elif rtype == 'Z':
+            # Zone (SOA)
+
+            # The defaults are very round numbers in hex...
+            # Note: Although the global default ttl is 86400 (1 day), the SOA
+            # TTL defaults to 2560, like the minttl value.
+            defaults = [None, "", "", timestr, "16384", "2048", "1048576", "2560", "2560", "0", None]
+            givenfields = line.split(':')
+            fields = overlay(givenfields, defaults)
+
+            name = fields[0]
+            primary = labels_to_dns(name_to_labels(fields[1]))
+            hostmaster = labels_to_dns(name_to_labels(fields[2]))
+            serial = int(fields[3])
+            refresh = int(fields[4])
+            retry = int(fields[5])
+            expire = int(fields[6])
+            minttl = int(fields[7])
+            ttl = int(fields[8])
+            ttd = int(fields[9],16)
+            loc = fields[10]
+            data = primary + hostmaster + u32_to_bytes(serial) + u32_to_bytes(refresh) + u32_to_bytes(retry) + u32_to_bytes(expire) + u32_to_bytes(minttl)
+            out.write(make_record(name, RR_TYPE_SOA, loc, ttl, ttd, data))
+        elif rtype == '&':
+            # NS record
+
+            # Note: default TTL for NS is 3 days
+            defaults = [None, "", "", "259200", "0", None]
+            givenfields = line.split(':')
+            fields = overlay(givenfields, defaults)
+
+            name = fields[0]
+            address = fields[1]
+            server = fields[2]
+            ttl = int(fields[3])
+            ttd = int(fields[4],16)
+            loc = fields[5]
+
+            if server == "":
+                server = "ns." + name
+            elif not '.' in server:
+                server = server + ".ns." + name
+
+            data = labels_to_dns(name_to_labels(server))
+            out.write(make_record(name, RR_TYPE_NS, loc, ttl, ttd, data))
+            if address != "":
+                data = u32_to_bytes(ipv4_to_u32(address))
+                out.write(make_record(server, RR_TYPE_A, loc, ttl, ttd, data))
+        elif rtype == '%':
+            raise Exception("% records are TBD")
+        else:
+            raise Exception("Unknown record type '{}'".format(rtype))
 
 
 # Finally, after the last record is an extra newline
